51ee5bc876bab9764dd178efca9c881671b9cfbf
hozaifa shaikh <hozaifask463@gmail.com>
2023-11-19 20:40:17 +0530

    learn fun&class Component

---
+++ client/src/Classcomp.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+class Classcomp extends Component
+{
+    render()
+        {
+        return(
+                <h1>Hello from class component</h1>
+            )
+        }
+}
+export default Classcomp;
