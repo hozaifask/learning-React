7fda7f4f1e88378fad2575b42f65633aedee9270
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-05 01:40:23 +0530

    add Toggle

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
-// import logo from './logo.svg';
+
 import './App.css';
 // import Funcomp from './Funcomp';
 // import Classcomp from './Classcomp';
 // import Event from './Event';
-import Form from "./Form";
-// import react,{useState} from 'react';
+// import Form from "./Form";
 // import  Student from "./Student"
+import Toggle from './Toggle';
 function App() {
  
-  // const[name,setName]=useState("shaikh");
+
 
   return (
     <div className="App">
-      <Form></Form>
+      {/* <Form></Form> */}
+      <Toggle></Toggle>
+
     
     
   
-      {/* <h1>Props in React</h1>
-      <Student name={name}/>
-      <button onClick={()=>{setName("Hozaifa")}}>click here to update</button> */}
-
-     
+       
     </div>
 
   );
