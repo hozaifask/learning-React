f83f6bd6658abc4fc36ff9b2d780dd52601b1433
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-05 01:14:31 +0530

    learn from handling  in react

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,11 +1,28 @@
-// import {useState} from "react";
+import {useState} from "react";
 function Form(){
-
-
-
+const[name,setName]=useState("");
+const[tnc,setTnc]=useState(false);
+const[interest,setInterest]=useState("");
+function getFormData(e)
+{
+    console.log(name,tnc,interest)
+    e.preventDefault()
+    setName("")
+}
     return(
       <>
       <h1>Handle Form in React</h1>
+      <form onSubmit={getFormData}>
+        <input type="text" placeholder="Enter Name" onChange={(e)=>setName(e.target.value)}></input><br></br>
+        <select onChange={(e)=>setInterest(e.target.value)}>
+            <option>Select Option</option>
+            <option>Marvel</option>
+            <option>DC</option>
+        </select><br></br>
+        <input type="checkbox" onChange={(e)=>setTnc(e.target.checked)}></input><span>Accept Term and Condition</span>
+        <br></br>
+         <button type="submit">Submit</button>
+      </form>
       
       </>
     )
