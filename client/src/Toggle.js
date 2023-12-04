c1628af8057a0db597b4a4c3b74df7bca4d9f213
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-05 01:40:46 +0530

    learn Toggle

---
+++ client/src/Toggle.js
@@ -0,0 +1,13 @@
+import {useState} from 'react'
+function Toggle(){
+    const [status,setStatus]=useState(true)
+    return(
+        <>
+        {
+            status?<h1>Hello world!</h1>:null
+        }
+        <button onClick={()=>setStatus(!status)}>Toggle </button>
+        </>
+    )
+}
+export default Toggle;
