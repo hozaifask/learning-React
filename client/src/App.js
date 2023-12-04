1e9743dc560b4c31637b2944ddad8f1e5af2e9ae
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-04 23:10:57 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 // import Funcomp from './Funcomp';
 // import Classcomp from './Classcomp';
 // import Event from './Event';
-import react,{useState} from 'react';
-import  Student from "./Student"
+import Form from "./Form";
+// import react,{useState} from 'react';
+// import  Student from "./Student"
 function App() {
-  const[name,setName]=useState("shaikh");
+ 
+  // const[name,setName]=useState("shaikh");
 
   return (
     <div className="App">
-      <h1>Props in React</h1>
+      <Form></Form>
+    
+    
+  
+      {/* <h1>Props in React</h1>
       <Student name={name}/>
-      <button onClick={()=>{setName("Hozaifa")}}>click here to update</button>
+      <button onClick={()=>{setName("Hozaifa")}}>click here to update</button> */}
 
      
     </div>
+
   );
 }
 export default App;
