dfe753c35970b3e447b356830a32b8737daaa138
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-06 14:04:42 +0530

    updated

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,10 +1,12 @@
 import {useState} from 'react';
 
 function Profile(){
-    const[loggedIn,setloggedIn]=useState(false);
+    const[loggedIn,setloggedIn]=useState(3);
+    
+    // three condition check 1,2,3
     return(
         <div>
-        {loggedIn?<h1>Welcome Hozaifa</h1>:<h1>Welcome Shaikh</h1>}
+        {loggedIn==1?<h1>Welcome 1 user</h1>:loggedIn==2?<h1>Welcome 2 user</h1>:<h1>Welcome 3 user</h1>}
     
        </div>
  )
