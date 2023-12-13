c2116c225523fae753fd93a1c9955f7bd5c878a2
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-13 20:41:31 +0530

    get value from input box

--- client/src/Getvalue.js
+++ client/src/Getvalue.js
@@ -1,4 +1,19 @@
-function Getvalue{
+function Getvalue(){
+
+function getData(val){
+    console.log(val.target.value)
+
+
+}
+    return (
+        <>
+        <h1>GET INPUT BOX VALUE</h1>
+        <input type="text"  onchange={getData}/>
+
+        
+        
+        </>
+    )
 
 }
 export default Getvalue;
