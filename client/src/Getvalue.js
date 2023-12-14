7b81cdc2ff55e1a5cbd67ff9b80d80260bea927e
hozaifa shaikh <hozaifask463@gmail.com>
2023-12-14 13:51:19 +0530

    updated here to print data by clicking on button

--- client/src/Getvalue.js
+++ client/src/Getvalue.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
 function Getvalue(){
     const[data,setData]=useState(null)
+    const[print,setPrint]=useState(false)
 
 function getData(val){
     console.log(val.target.value)
@@ -8,8 +9,13 @@ function getData(val){
 }
     return (
         <div>
-        <h1>{data}</h1>
+            {
+                print?
+                <h1>{data}</h1>
+                :null
+            }
         <input type="text"  onChange={getData}/>
+       <button onClick={()=>setPrint(true)}>Click here to print data</button> 
         </div>
         
         
