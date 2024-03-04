def16e3328407c1c535dbecc6b7e570364fd8c69
hozaifa shaikh <hozaifask463@gmail.com>
2024-03-05 00:31:23 +0530

    update

--- client/src/Event.js
+++ client/src/Event.js
@@ -1,36 +1,18 @@
-import React, { useState } from 'react';
-
-function Event (){
-    const[data,setData]=useState(0);
-  
-    function apple()
-    {
-         setData(data+1)
-        
-    }
-    return (
-        <div className="App">
-            <h1>{data}</h1>
-            <button onClick={apple}>Click me</button>
-        </div>    
-        );
-}
-export default Event;
 // import React, { useState } from 'react';
 
-// function Event() {
-//     const [data, setData] = useState(0);
-
-//     function apple() {
-//         setData(data + 1);
+// function Event (){
+//     const[data,setData]=useState(0);
+  
+//     function apple()
+//     {
+//          setData(data+1)
+        
 //     }
-
 //     return (
 //         <div className="App">
 //             <h1>{data}</h1>
 //             <button onClick={apple}>Click me</button>
-//         </div>
-//     );
+//         </div>    
+//         );
 // }
-
 // export default Event;
