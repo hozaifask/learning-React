17e659ef50bd08e87a8366a844a998b25e0ec2a7
hozaifa shaikh <hozaifask463@gmail.com>
2024-03-05 20:58:20 +0530

    updated

--- client/src/Student.js
+++ client/src/Student.js
@@ -1,7 +1,9 @@
 function Student(props){
     console.log(props)
     return(
+        <>
         <h1>hello {props.name} </h1>
+        <h3>email {props.email} </h3></>
     )
 }
 export default Student;
